eb600d32b235e6ab03c1a39d9a152b04af4d4386
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-10 08:24:28 +0600

    made team component

--- src/Pages/Home/Products/ProductCard/ProductCard.jsx
+++ src/Pages/Home/Products/ProductCard/ProductCard.jsx
@@ -1,6 +1,5 @@
-
-import { Rating } from '@smastrom/react-rating';
-import '@smastrom/react-rating/style.css'
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const ProductCard = ({ product }) => {
   const { img, product_name, price, rating } = product;
@@ -11,12 +10,15 @@ const ProductCard = ({ product }) => {
         <img src={img} alt="Service" className="w-full h-[200px]" />
       </figure>
       <div className="card-body text-center">
-        
         <div className="space-y-2 text-[#FF3811] font-semibold ">
-            <Rating className='mx-auto' readOnly={true} value={rating} style={{ maxWidth: 250 }} ></Rating>
-            <h2 className="font-bold text-center">{product_name}</h2>
+          <Rating
+            className="mx-auto"
+            readOnly={true}
+            value={rating}
+            style={{ maxWidth: 250 }}
+          ></Rating>
+          <h2 className="font-bold text-center">{product_name}</h2>
           <p>Price: ${price}</p>
-          
         </div>
       </div>
     </div>
