0981fc8cdf960438427aed35aa31d0dbb5929d73
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-12 16:57:52 +0600

    added sign up page

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import loginImg from "../../assets/images/login/login.svg";
+import signUpImg from "../../assets/images/login/login.svg";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 
-const Login = () => {
+const SignUp = () => {
 
     const [hidden, setHidden] = useState(true);
 
-    const handleLogin = e =>{
+    const handleSignUp = e =>{
         e.preventdefault();
     }
 
@@ -16,13 +16,13 @@ const Login = () => {
     <div className=" min-h-screen">
       <div className="flex my-auto justify-between items-center gap-4 flex-col lg:flex-row">
         <div className="text-center md:w-1/2 w-full">
-          <img src={loginImg} alt="" />
+          <img src={signUpImg} alt="" />
         </div>
         <div className="card bg-base-100 md:w-1/2 w-full ">
         
-          <form onSubmit={handleLogin} className="card-body">
+          <form onSubmit={handleSignUp} className="card-body">
             <div className="form-control">
-                <h2 className="text-4xl text-center font-bold text-[#ff3811]">Login Now!</h2>
+                <h2 className="text-4xl text-center font-bold text-[#ff3811]">Sign up Now!</h2>
             </div>
             <div className="form-control">
               <label className="label">
@@ -49,10 +49,10 @@ const Login = () => {
             </div>
             
             <div className="form-control text-center mt-4">
-                <p>New to here? Please <span className="text-[#ff3811] font-bold underline"><Link to='/signup'>Sign Up!</Link></span></p>
+                <p>Already have an account? Please <span className="text-[#ff3811] font-bold underline"><Link to='/login'>Login!</Link></span></p>
             </div>
             <div className="form-control mt-6">
-              <button className="btn bg-[#ff3811] text-white">Login</button>
+              <button className="btn bg-[#ff3811] text-white">Sign Up</button>
             </div>
           </form>
           <div className="relative -top-[195px]">
@@ -67,4 +67,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
