6d45615f70faf7687c60f17a1a2e4951c79adb3c
Md. Rafiul Islam <rislamshaon95@gmail.com>
2024-07-13 21:15:05 +0600

    made trapezoidal shape in service details page

--- src/Components/ServiceDetails/ServiceDetails.jsx
+++ src/Components/ServiceDetails/ServiceDetails.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from 'react-router-dom';
-
+import serviceImg from '../../assets/images/checkout/checkout.png'
+import './service.css';
 const ServiceDetails = () => {
 
     const serviceData = useLoaderData();
@@ -8,7 +9,11 @@ const ServiceDetails = () => {
 
     return (
         <div>
-            
+            <div className='relative'>
+                <img src={serviceImg} alt="service image" className='w-full' />
+                <h2 className='font-bold absolute top-[42%] left-12 text-white text-5xl '>Service Details</h2>
+                <div className='text-center text-white bg-[#ff3811] absolute bottom-0 left-[45%] font-medium trapezoid px-[52px] py-4'>Home/Service Details</div>
+            </div>
         </div>
     );
 };
